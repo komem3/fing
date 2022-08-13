03ac81c0141e7718a88e336cdfc4aa837e4e317e
Koumei Mikuni <mikuni-k@gicloud.co.jp>
2022-08-13 14:43:26 +0900

    optimize directory search #6 (#7)

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
 func main() {
-	if err := run(os.Args, os.Stdout, os.Stderr); err != nil {
-		fmt.Fprintln(os.Stderr, "\n", err)
-		os.Exit(1)
-	}
+	os.Exit(run(os.Args, os.Stdout, os.Stderr))
 }
